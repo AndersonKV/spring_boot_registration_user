82330bfe3276356214336a0eaad02b99c12cbe98
unknown <noitegarden@gmail.com>
2021-10-21 08:15:25 -0300

    project register user with email

---
+++ src/main/java/com/example/demo/registration/RegistrationController.java
@@ -0,0 +1,28 @@
+package com.example.demo.registration;
+
+import com.example.demo.appuser.AppUser;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+    private RegistrationService registrationService;
+
+    @PostMapping
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
+
+    @GetMapping(path = "confirm")
+    public String confirm(@RequestParam("token") String token) {
+        return registrationService.confirmToken(token);
+    }
+    @GetMapping
+    public List<AppUser> getUsers() {
+        return registrationService.getUsers();
+    }
+}
